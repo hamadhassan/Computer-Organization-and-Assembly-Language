afb6d2998884c2f52d3f85df7c12093a341ab83e
Muhammad Hammad Hassan <mhhjutt@gmail.com>
2022-11-02 17:53:07 +0500

    Update the code of Lab-1

--- Lab-1/Project32_VS2022/AddTwo.asm
+++ Lab-1/Project32_VS2022/AddTwo.asm
@@ -5,9 +5,15 @@
 .model flat,stdcall
 .stack 4096
 ExitProcess proto,dwExitCode:dword
-
+.data
+	list BYTE 10,20,30,40
+	myString BYTE "This is a long string."
+	Var2 BYTE 20 DUP(?)
+	ListSize = ($ - myString)
+	count=10
 .code
 main proc
+	mov eax,count
 	mov	eax,1
     mov ax,5
 	mov ah,3
